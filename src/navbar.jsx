d093ebdd12eda741e2e98c30ff1270d45e3b7c70
najiib5555 <najiibahmedmoha1559@gmail.com>
2025-01-05 11:00:13 +0300

    version13

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Navbar(){
     return(
-        <><div>happy new year boy</div></>
+        <><div>happy new year all my people</div></>
     )
 }
 export default Navbar
